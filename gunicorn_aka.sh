0a457f0a567129997df1af9baa34af5e24d82116
K Hong <k.hong@aol.com>
2016-09-21 23:12:11 -0700

    Angular front end and polling back end

--- gunicorn_aka.sh
+++ gunicorn_aka.sh
@@ -1,1 +1,3 @@
-exec gunicorn -w 4 -b 127.0.0.1:5001 aka:app
+source /home/k/MySites/akadrone/venv/bin/activate
+python /home/k/MySites/akadrone/worker.py &
+exec gunicorn -w 4 -b 127.0.0.1:5000 aka:app
